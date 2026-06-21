8b5532a1636169ba0bd550abf73186e25a01ccca
mairajkhan4356-star <mairajkhan4356@gmail.com>
2026-06-21 16:59:54 +0530

    Deleted ds-practical-1

--- ds-practical-1 - Copy/mains.py
+++
@@ -1,4 +0,0 @@
-import file_utils
-
-file_utils.write_file()
-file_utils.read_file()
--- ds-practical-1 - Copy/sDsd.py
+++
@@ -1,6 +0,0 @@
-try:
-    file = open("geek.txt", "r")
-    content = file.read()
-    print(content)
-finally:
-    file.close()
